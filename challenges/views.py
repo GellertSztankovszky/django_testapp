f365cf04854ef10b53782075daa5fed0742d478c
Gellért Sztankovszky <gellert.sztankovszky@foodautonomy.org>
2024-01-10 14:48:48 +0100

    use templates, register apps

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, reverse
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.template.loader import render_to_string
 
 # Create your views here.
 
@@ -45,7 +46,10 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_data = f"<h1>{challenge_text}</h1>"
+        response_data = render_to_string("challenges/challenge.html", {
+            "text": challenge_text,
+            "month_name": month
+        })
         return HttpResponse(response_data)
     except:
         return HttpResponseNotFound("<h1>This month is not supported!</h1>")
