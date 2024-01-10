412deb64d59bf797e32b00b3f2eae3b51c6abb43
Gellért Sztankovszky <gellert.sztankovszky@foodautonomy.org>
2024-01-10 13:41:10 +0100

    dynamic query params and redirects

--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,40 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 # Create your views here.
 
+monthly_challenges = {
+    "january": 'Start Running!',
+    "february": 'Eat no meat!',
+    "march": 'Learn Django!',
+    "april": 'Eat more vegetables!',
+    "may": "Eat less carbs!",
+    "june": 'Learn Python!',
+    "july": 'Learn Java!',
+    "august": 'Learn C!',
+    "september": 'Learn C++!',
+    "october": 'Learn C#!',
+    "november": 'Learn Ruby!',
+    "december": 'Stop Running!'
+}
+
+
 def index(request):
     return HttpResponse("challenges index")
+ 
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month")
+    
+    redirect_month = months[month-1]
+    return HttpResponseRedirect("/challenges/" + redirect_month)
+
+                        
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("This month is not supported!")
