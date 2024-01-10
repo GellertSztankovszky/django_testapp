d54da1999a44d2f1ed38547b6d517bb7094ad7cf
Gellért Sztankovszky <gellert.sztankovszky@foodautonomy.org>
2024-01-10 14:01:48 +0100

    return html

--- challenges/views.py
+++ challenges/views.py
@@ -26,7 +26,7 @@ def index(request):
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
     if month > len(months):
-        return HttpResponseNotFound("Invalid month")
+        return HttpResponseNotFound("<h1>Invalid month!</h1>")
     
     redirect_month = months[month-1]
     redirect_path = reverse("month-challenge", args=[redirect_month])
@@ -36,6 +36,7 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound("This month is not supported!")
+        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
