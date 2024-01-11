b7c3cb567954e520ef2b3a234dda2efb9929f4c7
Gellért Sztankovszky <gellert.sztankovszky@foodautonomy.org>
2024-01-11 17:42:27 +0100

    use model urls

--- book_outlet/urls.py
+++ book_outlet/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path, include
 from . import views
 
-urlpatterns = [path("", views.index), path("<int:id>", views.book_detail)]
+urlpatterns = [path("", views.index), path("<int:id>", views.book_detail, name="book-detail")]
